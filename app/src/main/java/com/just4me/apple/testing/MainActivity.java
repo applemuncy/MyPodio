885868f5446dbedda972b1555faf327106244c7a
apple muncy <j.apple.muncy@gmail.com>
2015-03-10 09:24:44 -0400

    Initial MyPodio project

---
+++ app/src/main/java/com/just4me/apple/testing/MainActivity.java
@@ -0,0 +1,59 @@
+package com.just4me.apple.testing;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.Toast;
+
+import com.podio.sdk.Session;
+import com.podio.sdk.Request.ResultListener;
+
+
+
+public class MainActivity extends ActionBarActivity {
+    private String TAG;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TAG = getClass().getName();
+        Log.d(TAG, "onCreate working");
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        Log.d(TAG, "onCreateOptionMenu working");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.d(TAG, "onOptionsItemSelected working");
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
---
+++ app/src/main/java/com/just4me/apple/testing/MyPodioApp.java
@@ -0,0 +1,31 @@
+package com.just4me.apple.testing;
+import android.util.Log;
+import android.app.Application;
+import com.podio.sdk.*;
+
+
+/**
+ * Created by apple on 3/8/2015.
+ */
+public class MyPodioApp extends Application {
+    private static final String TAG="MPodioApp";
+    private static final String CLIENT_ID = "mypodio-vgupqn";
+    private static final String CLIENT_SECRET = "f9m3ZpLGRIdKcil1Rficmvbe1JvoCZLPNy0Jpd3iEIbVpGWyXmu1vE1tuQgFGhwJ";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate start");
+        // Setup the Podio SDK runtime environment. Failing to call
+        // this method will cause any and all subsequent calls to
+        // throw exceptions.
+        try {
+            Podio.setup(this, CLIENT_ID, CLIENT_SECRET);
+            Log.d(TAG, "onCreate after Podio.setup");
+        }catch (Exception e )
+        {
+            Log.d (TAG, e.toString());
+        }
+    }
+
+}
